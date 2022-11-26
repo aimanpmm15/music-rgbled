2857d81323e11f77b347ab02f3b6fea634402fb5
aimanpmm15 <119101384+aimanpmm15@users.noreply.github.com>
2022-11-26 16:03:38 +0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,259 @@
+input.onButtonPressed(Button.A, function () {
+    edubitTrafficLightBit.setLed(LedColor.Red, edubitTrafficLightBit.digitalStatePicker(DigitalIoState.Off))
+    edubitTrafficLightBit.setLed(LedColor.Yellow, edubitTrafficLightBit.digitalStatePicker(DigitalIoState.Off))
+    edubitTrafficLightBit.setLed(LedColor.Green, edubitTrafficLightBit.digitalStatePicker(DigitalIoState.Off))
+})
+function i_will_follow_you () {
+    music.playTone(262, music.beat(BeatFraction.Half))
+    led.plotBarGraph(
+    edubitSoundBit.readSoundSensor(),
+    1023
+    )
+    music.playTone(294, music.beat(BeatFraction.Half))
+    led.plotBarGraph(
+    edubitSoundBit.readSoundSensor(),
+    1023
+    )
+    music.playTone(349, music.beat(BeatFraction.Half))
+    led.plotBarGraph(
+    edubitSoundBit.readSoundSensor(),
+    1023
+    )
+    music.playTone(392, music.beat(BeatFraction.Half))
+    led.plotBarGraph(
+    edubitSoundBit.readSoundSensor(),
+    1023
+    )
+    music.playTone(349, 2.5 * music.beat(BeatFraction.Whole))
+    led.plotBarGraph(
+    edubitSoundBit.readSoundSensor(),
+    1023
+    )
+    edubitTrafficLightBit.setLed(LedColor.Green, edubitTrafficLightBit.digitalStatePicker(DigitalIoState.On))
+    edubitTrafficLightBit.setLed(LedColor.Yellow, edubitTrafficLightBit.digitalStatePicker(DigitalIoState.Off))
+    edubitTrafficLightBit.setLed(LedColor.Red, edubitTrafficLightBit.digitalStatePicker(DigitalIoState.Off))
+    music.rest(music.beat(BeatFraction.Half))
+    music.playTone(262, music.beat(BeatFraction.Half))
+    led.plotBarGraph(
+    edubitSoundBit.readSoundSensor(),
+    1023
+    )
+    music.playTone(294, music.beat(BeatFraction.Half))
+    led.plotBarGraph(
+    edubitSoundBit.readSoundSensor(),
+    1023
+    )
+    music.playTone(349, music.beat(BeatFraction.Half))
+    led.plotBarGraph(
+    edubitSoundBit.readSoundSensor(),
+    1023
+    )
+    music.playTone(294, music.beat(BeatFraction.Half))
+    led.plotBarGraph(
+    edubitSoundBit.readSoundSensor(),
+    1023
+    )
+    music.playTone(349, music.beat(BeatFraction.Half))
+    led.plotBarGraph(
+    edubitSoundBit.readSoundSensor(),
+    1023
+    )
+    music.playTone(440, music.beat(BeatFraction.Half))
+    led.plotBarGraph(
+    edubitSoundBit.readSoundSensor(),
+    1023
+    )
+    music.playTone(523, 1.5 * music.beat(BeatFraction.Whole))
+    led.plotBarGraph(
+    edubitSoundBit.readSoundSensor(),
+    1023
+    )
+    music.playTone(440, music.beat(BeatFraction.Half))
+    led.plotBarGraph(
+    edubitSoundBit.readSoundSensor(),
+    1023
+    )
+    music.playTone(523, music.beat(BeatFraction.Double))
+    led.plotBarGraph(
+    edubitSoundBit.readSoundSensor(),
+    1023
+    )
+    music.rest(music.beat(BeatFraction.Half))
+    edubitTrafficLightBit.setLed(LedColor.Green, edubitTrafficLightBit.digitalStatePicker(DigitalIoState.On))
+    edubitTrafficLightBit.setLed(LedColor.Yellow, edubitTrafficLightBit.digitalStatePicker(DigitalIoState.On))
+    edubitTrafficLightBit.setLed(LedColor.Red, edubitTrafficLightBit.digitalStatePicker(DigitalIoState.Off))
+    music.playTone(523, music.beat(BeatFraction.Half))
+    led.plotBarGraph(
+    edubitSoundBit.readSoundSensor(),
+    1023
+    )
+    music.playTone(587, music.beat(BeatFraction.Half))
+    led.plotBarGraph(
+    edubitSoundBit.readSoundSensor(),
+    1023
+    )
+    music.playTone(587, music.beat(BeatFraction.Half))
+    led.plotBarGraph(
+    edubitSoundBit.readSoundSensor(),
+    1023
+    )
+    music.playTone(587, music.beat(BeatFraction.Half))
+    led.plotBarGraph(
+    edubitSoundBit.readSoundSensor(),
+    1023
+    )
+    music.playTone(587, music.beat(BeatFraction.Half))
+    led.plotBarGraph(
+    edubitSoundBit.readSoundSensor(),
+    1023
+    )
+    music.playTone(440, music.beat(BeatFraction.Half))
+    led.plotBarGraph(
+    edubitSoundBit.readSoundSensor(),
+    1023
+    )
+    music.playTone(587, music.beat(BeatFraction.Half))
+    led.plotBarGraph(
+    edubitSoundBit.readSoundSensor(),
+    1023
+    )
+    music.playTone(523, music.beat(BeatFraction.Half))
+    led.plotBarGraph(
+    edubitSoundBit.readSoundSensor(),
+    1023
+    )
+    music.rest(music.beat(BeatFraction.Half))
+    edubitTrafficLightBit.setLed(LedColor.Green, edubitTrafficLightBit.digitalStatePicker(DigitalIoState.On))
+    edubitTrafficLightBit.setLed(LedColor.Yellow, edubitTrafficLightBit.digitalStatePicker(DigitalIoState.On))
+    edubitTrafficLightBit.setLed(LedColor.Red, edubitTrafficLightBit.digitalStatePicker(DigitalIoState.On))
+    music.playTone(523, music.beat(BeatFraction.Half))
+    led.plotBarGraph(
+    edubitSoundBit.readSoundSensor(),
+    1023
+    )
+    music.playTone(587, music.beat(BeatFraction.Half))
+    led.plotBarGraph(
+    edubitSoundBit.readSoundSensor(),
+    1023
+    )
+    music.playTone(587, music.beat(BeatFraction.Half))
+    led.plotBarGraph(
+    edubitSoundBit.readSoundSensor(),
+    1023
+    )
+    music.playTone(587, music.beat(BeatFraction.Half))
+    led.plotBarGraph(
+    edubitSoundBit.readSoundSensor(),
+    1023
+    )
+    music.playTone(587, music.beat(BeatFraction.Half))
+    led.plotBarGraph(
+    edubitSoundBit.readSoundSensor(),
+    1023
+    )
+    music.playTone(523, music.beat(BeatFraction.Half))
+    led.plotBarGraph(
+    edubitSoundBit.readSoundSensor(),
+    1023
+    )
+    music.playTone(494, music.beat(BeatFraction.Half))
+    led.plotBarGraph(
+    edubitSoundBit.readSoundSensor(),
+    1023
+    )
+    music.playTone(523, music.beat(BeatFraction.Double))
+    led.plotBarGraph(
+    edubitSoundBit.readSoundSensor(),
+    1023
+    )
+    music.rest(music.beat(BeatFraction.Half))
+    edubitTrafficLightBit.setLed(LedColor.Red, edubitTrafficLightBit.digitalStatePicker(DigitalIoState.Off))
+    edubitTrafficLightBit.setLed(LedColor.Yellow, edubitTrafficLightBit.digitalStatePicker(DigitalIoState.On))
+    edubitTrafficLightBit.setLed(LedColor.Green, edubitTrafficLightBit.digitalStatePicker(DigitalIoState.On))
+    music.playTone(523, music.beat(BeatFraction.Half))
+    led.plotBarGraph(
+    edubitSoundBit.readSoundSensor(),
+    1023
+    )
+    music.playTone(440, music.beat(BeatFraction.Whole))
+    led.plotBarGraph(
+    edubitSoundBit.readSoundSensor(),
+    1023
+    )
+    music.playTone(392, music.beat(BeatFraction.Whole))
+    led.plotBarGraph(
+    edubitSoundBit.readSoundSensor(),
+    1023
+    )
+    edubitTrafficLightBit.setLed(LedColor.Yellow, edubitTrafficLightBit.digitalStatePicker(DigitalIoState.Off))
+    music.playTone(440, music.beat(BeatFraction.Whole))
+    led.plotBarGraph(
+    edubitSoundBit.readSoundSensor(),
+    1023
+    )
+    music.playTone(392, music.beat(BeatFraction.Whole))
+    led.plotBarGraph(
+    edubitSoundBit.readSoundSensor(),
+    1023
+    )
+    music.playTone(349, 2.5 * music.beat(BeatFraction.Whole))
+    led.plotBarGraph(
+    edubitSoundBit.readSoundSensor(),
+    1023
+    )
+    edubitTrafficLightBit.setLed(LedColor.Green, edubitTrafficLightBit.digitalStatePicker(DigitalIoState.Off))
+    music.rest(music.beat(BeatFraction.Whole))
+    led.plotBarGraph(
+    edubitSoundBit.readSoundSensor(),
+    1023
+    )
+}
+input.onButtonPressed(Button.B, function () {
+    edubitRgbBit.setPixelColor(0, 0xff0000)
+    edubitRgbBit.setPixelColor(1, 0xff0000)
+    edubitRgbBit.setPixelColor(2, 0xff0000)
+    edubitRgbBit.setPixelColor(3, 0xff0000)
+})
+basic.showString("Hello!")
+basic.forever(function () {
+    while (edubitIrBit.isIrSensorTriggered()) {
+        i_will_follow_you()
+    }
+    while (!(edubitIrBit.isIrSensorTriggered())) {
+        basic.showLeds(`
+            . . # . .
+            . # . # .
+            . # # # .
+            . # . # .
+            . # . # .
+            `)
+        basic.showLeds(`
+            # # # # #
+            . . # . .
+            . . # . .
+            . . # . .
+            # # # # #
+            `)
+        basic.showLeds(`
+            # # # # #
+            # . # . #
+            # . # . #
+            # . # . #
+            # . # . #
+            `)
+        basic.showLeds(`
+            . . # . .
+            . # . # .
+            . # # # .
+            . # . # .
+            . # . # .
+            `)
+        basic.showLeds(`
+            # . . . #
+            # # . . #
+            # . # . #
+            # . . # #
+            # . . . #
+            `)
+    }
+})
